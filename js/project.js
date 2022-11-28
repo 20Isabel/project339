5339ba6b3cecd7430c64c673a7e24ae0dfc18b0c
Tamariah Davis <112430319+tamarida@users.noreply.github.com>
2022-11-28 09:52:48 -0500

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,11 @@
+document.querySelector("#style1").addEventListener("click", function(){
+    document.querySelector("img").classList.toggle("style1")
+})
+
+document.querySelector("#style2").addEventListener("click", function(){
+    document.querySelector("img").classList.toggle("style2")
+})
+
+document.querySelector("#style3").addEventListener("click", function(){
+    document.querySelector("img").classList.toggle("style3")
+})
